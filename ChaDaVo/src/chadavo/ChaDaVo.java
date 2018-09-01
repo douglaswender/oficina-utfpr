20122719f15578c012c84a7a13b6395e6ceb5da5
VBelloli <vbelloli@outlook.com>
2018-08-31 21:05:33 -0300

    Primeiro Commit

--- ChaDaVo/src/chadavo/ChaDaVo.java
+++ ChaDaVo/src/chadavo/ChaDaVo.java
@@ -16,7 +16,7 @@ public class ChaDaVo {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Belloli Commit");
+        System.out.println("Belloli");
     }
     
 }
