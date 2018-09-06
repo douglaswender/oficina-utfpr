1493b20bb0cd68744dc23324b8713207770668b7
douglaswender <douglas.ifro@gmail.com>
2018-09-05 23:49:35 -0300

    novo commit das telas

---
+++ chadavo/src/chadavo/controller/TelaInicioController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chadavo.controller;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author dougl
+ */
+public class TelaInicioController implements Initializable {
+    
+    @FXML
+    private JFXButton btnLogin;
+
+    @FXML
+    private JFXButton btnCadastro;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
